511ad0df705112baf0cb9bab2cc62ee532341042
Jeff Graves <jgraves4@ggc.edu>
2017-09-17 13:34:14 -0400

    update

--- 12-add_pixels.sh
+++ 12-add_pixels.sh
@@ -4,12 +4,11 @@ echo "Enter your first numbsder"
 read first
 echo "Enter your second number"
 read second
-echo "Enter your third number"
-read third
 
-sum=$(( first+second+third ))
 
-echo "The sum is $sum"
+sum=$(( first*second ))
+
+echo "The number of pixels is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
